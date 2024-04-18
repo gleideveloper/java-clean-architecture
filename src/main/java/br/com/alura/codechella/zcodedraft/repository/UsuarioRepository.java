c022026233e0dc4ab1697141c9e79ab0c267803f
Gleides Vinente <gleidevelop@gmail.com>
2024-04-17 22:48:21 -0300

    Removendo o pardão MVC

--- src/main/java/br/com/alura/codechella/zcodedraft/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.codechella.zcodedraft.repository;
-
-import br.com.alura.codechella.zcodedraft.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
